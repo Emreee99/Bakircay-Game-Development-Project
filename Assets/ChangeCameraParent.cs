c731dea990f5d6e99a9c82500816a93797cd2d88
Emre Turan <61634286+Emreee99@users.noreply.github.com>
2023-01-22 23:15:40 +0300

    Update ChangeCameraParent.cs

--- Assets/ChangeCameraParent.cs
+++ Assets/ChangeCameraParent.cs
@@ -5,30 +5,24 @@ using UnityEngine.UI;
 
 public class ChangeCameraParent : MonoBehaviour
 {
-    public int requiredItems = 0; // The number of items required to change the camera parent
-    public GameObject car; // The car to change the camera parent to
-    public GameObject player; // The player to change the camera parent from
+    public int requiredItems = 0; 
+    public GameObject car; 
+    public GameObject player; 
 
-    private Camera mainCamera; // The main camera
-    //private int inventory = 5; // The player's inventory
+    private Camera mainCamera; 
     public Text totalItem;
 
     void Start()
     {
-        // Get the main camera
         mainCamera = Camera.main;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        // Check if the player entered the trigger area
         if (other.transform == player.transform)
         {
-            
-            // Check if the player has enough items in their inventory
             if (player.GetComponent<PickUpItem>().inventory >= requiredItems)
             {
-                // Change the camera parent
                 mainCamera.transform.SetParent(car.transform);
                 player.GetComponent<PlayerMovement>().enabled = false;
                 car.GetComponent<CarMovement>().enabled = true;
