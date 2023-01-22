23fadf5f38a5e0626d3fea33ef956c89dbe948a8
Emre Turan <61634286+Emreee99@users.noreply.github.com>
2023-01-22 23:18:09 +0300

    Update PickUpItem.cs

--- Assets/PickUpItem.cs
+++ Assets/PickUpItem.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class PickUpItem : MonoBehaviour
 {
-    public float pickupDistance = 1f; // The distance at which the character can pick up the item
-    public LayerMask pickupLayer; // The layer of the item to pick up
+    public float pickupDistance = 1f;
+    public LayerMask pickupLayer;
 
-    private Transform item; // The item to pick up
-    private Transform player; // The character's transform
-    public int inventory = 0; // The character's inventory
+    private Transform item;
+    private Transform player;
+    public int inventory = 0;
     public Text totalItem;
 
 
@@ -23,35 +23,28 @@ public class PickUpItem : MonoBehaviour
 
     void Start()
     {
-        // Get the character's transform
         player = transform;
     }
 
     void Update()
     {
-        // Check if the player is close to an item
         if (item != null)
         {
-            // Check if the player is close enough to pick up the item
             if (Vector3.Distance(player.position, item.position) < pickupDistance)
             {
-                // Pick up the item
                 PickUp();
             }
         }
         else
         {
-            // Find the nearest item
             FindNearestItem();
         }
     }
 
     void FindNearestItem()
     {
-        // Find all items in the scene
         GameObject[] items = GameObject.FindGameObjectsWithTag("Material");
 
-        // Find the nearest item
         float minDistance = float.MaxValue;
         foreach (GameObject itemObject in items)
         {
@@ -67,14 +60,9 @@ public class PickUpItem : MonoBehaviour
 
     void PickUp()
     {
-        // Add the item to the character's inventory
         inventory++;
         totalItem.text = inventory.ToString();
 
-        // Do something with the inventory (e.g., display the new inventory count)
-        // ...
-
-        // Destroy the item
         Destroy(item.gameObject);
         item = null;
     }
