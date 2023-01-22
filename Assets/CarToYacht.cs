f2632fa16ee7129b34b2d9cbb57cfc250cb3cab4
Emre Turan <61634286+Emreee99@users.noreply.github.com>
2023-01-22 23:14:35 +0300

    Update CarToYacht.cs

--- Assets/CarToYacht.cs
+++ Assets/CarToYacht.cs
@@ -5,30 +5,24 @@ using UnityEngine.UI;
 
 public class CarToYacht : MonoBehaviour
 {
-    public int requiredItems = 0; // The number of items required to change the camera parent
-    public GameObject yacht; // The car to change the camera parent to
-    public GameObject car; // The player to change the camera parent from
+    public int requiredItems = 0;
+    public GameObject yacht;
+    public GameObject car;
 
-    private Camera mainCamera; // The main camera
-    //private int inventory = 5; // The player's inventory
+    private Camera mainCamera;
     public Text totalItem;
 
     void Start()
     {
-        // Get the main camera
         mainCamera = Camera.main;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        // Check if the player entered the trigger area
         if (other.transform == car.transform)
         {
-
-            // Check if the player has enough items in their inventory
             if (car.GetComponent<PickUpItem>().inventory >= requiredItems)
             {
-                // Change the camera parent
                 mainCamera.transform.SetParent(yacht.transform);
                 car.GetComponent<CarMovement>().enabled = false;
                 yacht.GetComponent<YachtMovement>().enabled = true;
