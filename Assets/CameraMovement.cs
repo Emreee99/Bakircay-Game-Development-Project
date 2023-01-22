72da58abeca3a4516f6a8c9670a3937eda289712
Emre Turan <61634286+Emreee99@users.noreply.github.com>
2023-01-22 23:13:12 +0300

    Update CameraMovement.cs

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform target; // the transform of the object to follow
-    public float smoothTime = 0.3f; // the time it takes for the camera to catch up with the target's movement
-    public Vector3 offset; // the distance between the camera and the target
+    public Transform target;
+    public float smoothTime = 0.3f;
+    public Vector3 offset;
 
-    private Vector3 velocity = Vector3.zero; // the velocity at which the camera is moving
+    private Vector3 velocity = Vector3.zero;
 
     void LateUpdate()
     {
-        // set the position of the camera to the target's position, offset by the desired distance
         Vector3 desiredPosition = target.position + offset;
         transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
     }
