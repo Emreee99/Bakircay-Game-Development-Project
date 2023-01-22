ef79f44c6f4fbea4e176f62a9bca11ef5bbcb18c
Emre Turan <61634286+Emreee99@users.noreply.github.com>
2023-01-22 23:20:24 +0300

    Update YachtToTrain.cs

--- Assets/YachtToTrain.cs
+++ Assets/YachtToTrain.cs
@@ -5,27 +5,22 @@ using UnityEngine.UI;
 
 public class YachtToTrain : MonoBehaviour
 {
-    public int requiredItems = 0; // The number of items required to change the camera parent
-    public GameObject train; // The car to change the camera parent to
-    public GameObject yacht; // The player to change the camera parent from
+    public int requiredItems = 0;
+    public GameObject train;
+    public GameObject yacht;
 
-    private Camera mainCamera; // The main camera
-    //private int inventory = 5; // The player's inventory
+    private Camera mainCamera;
     public Text totalItem;
 
     void Start()
     {
-        // Get the main camera
         mainCamera = Camera.main;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        // Check if the player entered the trigger area
         if (other.transform == yacht.transform)
         {
-
-            // Check if the player has enough items in their inventory
             if (yacht.GetComponent<PickUpItem>().inventory >= requiredItems)
             {
                 
